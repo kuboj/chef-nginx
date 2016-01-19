6e5c0cbe371d5fddade9e29203b84b83717345d7
Jakub Jursa <jju@rsd.com>
2016-01-19 13:23:48 +0100

    Revert "fix"
    This reverts commit 26c055f392ce1a9ffcf31f88dc5100566c5d1111.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,14 +3,14 @@
 # Recipe:: default
 #
 
-#repo = node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])
+repo = node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])
 
 apt_repository "nginx" do
-  uri node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["uri"]
+  uri repo["uri"]
   distribution repo["distribution"]
   components repo["components"]
-  key node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["key"]
-  keyserver node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["keyserver"] if node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["keyserver"]
-  deb_src node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["deb_src"]
+  key repo["key"]
+  keyserver repo["keyserver"] if repo["keyserver"]
+  deb_src repo["deb_src"]
   action :add
 end
