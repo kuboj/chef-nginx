26c055f392ce1a9ffcf31f88dc5100566c5d1111
Jakub Jursa <jju@rsd.com>
2016-01-19 13:17:52 +0100

    fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,14 +3,14 @@
 # Recipe:: default
 #
 
-repo = node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])
+#repo = node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])
 
 apt_repository "nginx" do
-  uri repo["uri"]
+  uri node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["uri"]
   distribution repo["distribution"]
   components repo["components"]
-  key repo["key"]
-  keyserver repo["keyserver"] if repo["keyserver"]
-  deb_src repo["deb_src"]
+  key node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["key"]
+  keyserver node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["keyserver"] if node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["keyserver"]
+  deb_src node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])["deb_src"]
   action :add
 end
